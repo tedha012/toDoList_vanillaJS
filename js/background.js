ceef95b5ce590f6df78d16f3721319b04a578f8f
tedha012 <tedha012@gmail.com>
2022-10-04 19:42:28 +0900

    addBackground

---
+++ js/background.js
@@ -0,0 +1,14 @@
+const images = [
+    "0.png",
+    "1.jpg",
+    "2.png",
+    "3.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+// console.log(bgImage);
+
+document.body.appendChild(bgImage);
