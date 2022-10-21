d3e0dcb90b8d1ebfc431d5dd9c37e54391428617
tedha012 <tedha012@gmail.com>
2022-10-21 17:10:57 +0900

    add css

--- js/background.js
+++ js/background.js
@@ -1,8 +1,18 @@
 const images = [
-    "0.png",
-    "1.jpg",
-    "2.png",
-    "3.jpg"
+  "001.jpg",
+  "002.jpg",
+  "003.jpg",
+  "004.jpg",
+  "005.jpg",
+  "006.jpg",
+  "007.jpg",
+  "008.jpg",
+  "009.jpg",
+  "010.jpg",
+  "011.jpg",
+  "012.jpg",
+  "013.jpg",
+  "014.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
--- js/quotes.js
+++ js/quotes.js
@@ -1,46 +1,61 @@
 const quotes = [
-    {
-      quote: "The way to get started is to quit talking and begin doing.",
-      author: "Walt Disney",
-    },
-    {
-      quote: "Life is what happens when you're busy making other plans.",
-      author: "John Lennon",
-    },
-    {
-      quote:
-        "The world is a book and those who do not travel read only one page.",
-      author: "Saint Augustine",
-    },
-    {
-      quote: "Life is either a daring adventure or nothing at all.",
-      author: "Helen Keller",
-    },
-    {
-      quote: "To Travel is to Live",
-      author: "Hans Christian Andersen",
-    },
-    {
-      quote: "Only a life lived for others is a life worthwhile.",
-      author: "Albert Einstein",
-    },
-    {
-      quote: "You only live once, but if you do it right, once is enough.",
-      author: "Mae West",
-    },
-    {
-      quote: "Never go on trips with anyone you do ntot love.",
-      author: "Hemmingway",
-    },
-    {
-      quote: "We wander for distraction, but we travel for fulfilment.",
-      author: "Hilaire Belloc",
-    },
-    {
-      quote: "Travel expands the mind and fills the gap.",
-      author: "Sheda Savage",
-    },
-  ];
+  {
+    quote: "사람들이 뭐라고 하든 자신이 옳다고 믿는 길이 최선의 길입니다",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "상대방의 마음이 움직이기만 기다리지 말고 먼저 다가가보세요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "자신을 바꿀수 있는 사람은 오직 자기 자신밖에 없습니다",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "내가 힘들다고 다른 사람을 탓하지 마세요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "어설프게 아느니 차라리 모르는 편이 나아요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "적어도 스스로에게는 정직하세요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "눈앞의 행복을 잡아요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "작은 행복이 쌓이고 쌓여 큰 행복이 되요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "세상에 휩쓸리지 않는 방법은 잠시 멈추는 거예요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "이미 선택한 것에 미련을 두지 마세요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "첫 번째는 나를 사랑하는 거예요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "누구나 자기 자신을 가장 사랑합니다",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "친해지고 싶은 사람이 있다면 일단 말을 걸어보세요",
+    author: "곰돌이 푸",
+  },
+  {
+    quote: "나의 길은 나만이 정할 수 있어요",
+    author: "곰돌이 푸",
+  },
+];
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
